5d2c71f4e78162a65db8f61df139d1204dc100ce
Zeropoet <mancel@zeropoet.xyz>
2026-02-05 22:35:46 -0500

    *🜂

--- sketch.js
+++ sketch.js
@@ -2,6 +2,10 @@ let velas = [];
 let sun;
 let cnv;
 let showCrosshair = false;
+let crosshairPos;
+let crosshairVel;
+const crosshairSpeed = 0.35;
+const crosshairMaxSpeed = 6;
 
 function setup() {
   applyPixelDensity();
@@ -16,7 +20,9 @@ function setup() {
 
   background(220);
 
-  sun = new Vela(width / 2, height / 2, 0, 0, 300);
+  sun = new Vela(width / 2, height / 2, 0, 0, 1000, true);
+  crosshairPos = createVector(width / 2, height / 2);
+  crosshairVel = createVector(0, 0);
 
   for (let i = 0; i < 10; i++) {
     let x = random(width);
@@ -30,9 +36,7 @@ function setup() {
 function draw() {
   background(220, 90);
 
-  if (showCrosshair) {
-    //blendMode(DIFFERENCE);
-  }
+  sun.show();
 
   for (let vela of velas) {
     sun.attract(vela);
@@ -45,8 +49,11 @@ function draw() {
 
   for (let vela of velas) {
     vela.update();
+    vela.showTrail();
     vela.show();
   }
+
+  updateCrosshair();
 
   if (showCrosshair) {
     blendMode(BLEND);
@@ -54,14 +61,14 @@ function draw() {
     stroke(255, 0, 0, 200);
     strokeWeight(1);
     noFill();
-    let cx = width / 2;
-    let cy = height / 2;
+    let cx = crosshairPos.x;
+    let cy = crosshairPos.y;
     line(cx - 6, cy, cx + 6, cy);
     line(cx, cy - 6, cx, cy + 6);
     pop();
   }
 
-  //sun.show();
+
 }
 
 function keyPressed() {
@@ -73,9 +80,36 @@ function keyPressed() {
 function windowResized() {
   applyPixelDensity();
   resizeCanvas(windowWidth, windowHeight);
+  crosshairPos.set(width / 2, height / 2);
 }
 
 function applyPixelDensity() {
   const dpr = window.devicePixelRatio || 1;
   pixelDensity(Math.min(dpr, 2));
+}
+
+function updateCrosshair() {
+  if (!crosshairPos || !crosshairVel) return;
+
+  let ax = 0;
+  let ay = 0;
+  if (keyIsDown(LEFT_ARROW)) ax -= 1;
+  if (keyIsDown(RIGHT_ARROW)) ax += 1;
+  if (keyIsDown(UP_ARROW)) ay -= 1;
+  if (keyIsDown(DOWN_ARROW)) ay += 1;
+
+  if (ax !== 0 || ay !== 0) {
+    let accel = createVector(ax, ay).setMag(crosshairSpeed);
+    crosshairVel.add(accel);
+  } else {
+    crosshairVel.mult(0.985);
+  }
+
+  crosshairVel.limit(crosshairMaxSpeed);
+  crosshairPos.add(crosshairVel);
+
+  if (crosshairPos.x < 0) crosshairPos.x = width;
+  if (crosshairPos.x > width) crosshairPos.x = 0;
+  if (crosshairPos.y < 0) crosshairPos.y = height;
+  if (crosshairPos.y > height) crosshairPos.y = 0;
 }
--- velas.js
+++ velas.js
@@ -1,10 +1,13 @@
 class Vela {
-  constructor(x, y, vx, vy, m) {
+  constructor(x, y, vx, vy, m, isSun = false) {
     this.pos = createVector(x, y);
+    this.prev = this.pos.copy();
     this.vel = createVector(vx, vy);
     this.acc = createVector(0, 0);
     this.mass = m;
-    this.r = sqrt(this.mass);
+    this.baseR = sqrt(this.mass);
+    this.r = this.baseR;
+    this.isSun = isSun;
   }
 
   applyForce(force) {
@@ -19,16 +22,39 @@ class Vela {
     let strength = ((this.mass * vela.mass) / distanceSq) * G;
     force.setMag(strength);
     vela.applyForce(force);
+
+    if (this.isSun) {
+      let d = sqrt(distanceSq);
+      let t = 1 - (d - 10) / (sqrt(1000) - 10);
+      t = constrain(t, 0, 1);
+      let swell = (t * t) * 18;
+      let targetR = this.baseR + swell;
+      this.r = lerp(this.r, targetR, 0.12);
+    }
   }
 
   update() {
+    this.prev.set(this.pos);
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
   }
 
+  showTrail() {
+    if (abs(this.pos.x - this.prev.x) > width / 2) return;
+    if (abs(this.pos.y - this.prev.y) > height / 2) return;
+    stroke(255, 20);
+    strokeWeight(3);
+    line(this.prev.x, this.prev.y, this.pos.x, this.pos.y);
+  }
+
   show() {
-    fill(0);
+    if (this.isSun) {
+      noFill();
+      stroke(255);
+    } else {
+      fill(0);
+    }
     ellipse(this.pos.x, this.pos.y, this.r * 2);
   }
 }
